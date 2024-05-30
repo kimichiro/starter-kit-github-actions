30354b66bc09ca6572d621b5b06e190a664dd70c
kimichiro <klimaxone@gmail.com>
2024-05-30 16:07:01 +0700

    correct bumping logic

--- .github/actions/bump-semantic-version/__tests__/version.test.ts
+++ .github/actions/bump-semantic-version/__tests__/version.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, jest, test } from '@jest/globals'
+import { EOL } from 'os'
 
-import * as version from '../src/version'
-import { bumpVersion, findMostRecentVersion } from '../src/version'
-import { Prerelease, PrereleaseId, TagPrefix, Type } from '../src/input'
+import { Type, Prerelease, PrereleaseId } from '../src/input'
+import { bumpVersion, findVersions } from '../src/version'
 
 jest.mock('@actions/core')
 jest.mock('simple-git', () => ({
@@ -11,113 +11,128 @@ jest.mock('simple-git', () => ({
 }))
 
 const mockSimpleGit = {
-    raw: jest.fn(),
-    tag: jest.fn(() => Promise.resolve('')),
+    raw: jest.fn(() => Promise.resolve('')),
 }
 
-const testCases = {
-    findMostRecentVersion: [
-        ['', [''], undefined],
-        ['', ['1.0.0'], '1.0.0'],
-        ['', ['1.0.0', '1.1.0-rc.0'], '1.1.0-rc.0'],
-        ['', ['1.0.0', '1.1.0-rc.0', '1.1.0'], '1.1.0'],
-        ['release/', ['release/1.0.0', 'release/1.1.0-rc.0'], '1.1.0-rc.0'],
-        ['release/', ['release/1.0.0', 'release/2.0.0'], '2.0.0'],
-        ['release/', ['1.0.0', 'release/2.0.0'], '2.0.0'],
-        ['release/', ['2.1.0-rc.0', 'release/2.0.0'], '2.0.0'],
-        ['release/', ['3.0.0', 'release/2.0.0'], '2.0.0'],
-    ],
-    bumpVersion: [
-        ['patch', true, 'rc', 'release/', '', undefined, undefined],
+describe('findVersions()', () => {
+    test.each([
+        [[''], [''], []],
+        [[''], ['1.0.0'], ['1.0.0']],
+        [[''], ['1.0.0', '1.1.0-rc.0'], ['1.0.0', '1.1.0-rc.0']],
+        [[''], ['1.0.0', '1.1.0-rc.0', 'release/1.1.0', 'release/1.2.0-rc.0'], ['1.0.0', '1.1.0-rc.0']],
+        [[''], ['release/1.1.0', 'release/1.2.0-rc.0'], []],
+        [['release/'], [''], []],
+        [['release/'], ['2.0.0'], []],
+        [['release/'], ['release/2.0.0'], ['2.0.0']],
+        [['release/'], ['2.0.0', '2.1.0-rc.0'], []],
+        [['release/'], ['2.0.0', '2.1.0-rc.0', 'release/2.1.0', 'release/2.2.0-rc.0'], ['2.1.0', '2.2.0-rc.0']],
+        [['release/'], ['release/2.1.0', 'release/2.2.0-rc.0'], ['2.1.0', '2.2.0-rc.0']],
+    ])('when args=%j and following versions found %p then returns %p', async ([tagPrefix], foundTags, expected) => {
+        const tagResult = (foundTags as string[]).join(EOL)
+        mockSimpleGit.raw.mockResolvedValueOnce('')
+        mockSimpleGit.raw.mockResolvedValueOnce('')
+        mockSimpleGit.raw.mockResolvedValueOnce(tagResult)
 
-        ['patch', true, 'rc', 'release/', '0.0.0', undefined, '0.0.1-rc.0'],
-        ['minor', true, 'rc', 'release/', '0.0.0', undefined, '0.1.0-rc.0'],
-        ['major', true, 'rc', 'release/', '0.0.0', undefined, '1.0.0-rc.0'],
+        const result = await findVersions(tagPrefix)
 
-        ['patch', true, 'rc', 'release/', '0.0.0', '1.0.0', '1.0.1-rc.0'],
-        ['minor', true, 'rc', 'release/', '0.0.0', '1.0.0', '1.1.0-rc.0'],
-        ['major', true, 'rc', 'release/', '0.0.0', '1.0.0', '2.0.0-rc.0'],
-        ['patch', true, 'rc', 'release/', '0.0.0', '1.2.3', '1.2.4-rc.0'],
-        ['minor', true, 'rc', 'release/', '0.0.0', '1.2.3', '1.3.0-rc.0'],
-        ['major', true, 'rc', 'release/', '0.0.0', '1.2.3', '2.0.0-rc.0'],
-        ['patch', true, 'rc', 'release/', '0.0.0', '1.0.0-rc.0', '1.0.0-rc.1'],
-        ['minor', true, 'rc', 'release/', '0.0.0', '1.0.0-rc.0', '1.0.0-rc.1'],
-        ['major', true, 'rc', 'release/', '0.0.0', '1.0.0-rc.0', '1.0.0-rc.1'],
-        ['patch', true, 'rc', 'release/', '0.0.0', '1.2.3-rc.4', '1.2.3-rc.5'],
-        ['minor', true, 'rc', 'release/', '0.0.0', '1.2.3-rc.4', '1.2.3-rc.5'],
-        ['major', true, 'rc', 'release/', '0.0.0', '1.2.3-rc.4', '1.2.3-rc.5'],
-
-        ['patch', true, 'rc', 'release/', '0.0.0', '1.0.0-alpha.0', '1.0.0-rc.0'],
-        ['minor', true, 'rc', 'release/', '0.0.0', '1.0.0-alpha.0', '1.0.0-rc.0'],
-        ['major', true, 'rc', 'release/', '0.0.0', '1.0.0-alpha.0', '1.0.0-rc.0'],
-        ['patch', true, 'rc', 'release/', '0.0.0', '1.2.3-alpha.4', '1.2.3-rc.0'],
-        ['minor', true, 'rc', 'release/', '0.0.0', '1.2.3-alpha.4', '1.2.3-rc.0'],
-        ['major', true, 'rc', 'release/', '0.0.0', '1.2.3-alpha.4', '1.2.3-rc.0'],
-        ['patch', true, 'rc', 'release/', '0.0.0', '1.0.0-alpha', '1.0.0-rc.0'],
-        ['minor', true, 'rc', 'release/', '0.0.0', '1.0.0-alpha', '1.0.0-rc.0'],
-        ['major', true, 'rc', 'release/', '0.0.0', '1.0.0-alpha', '1.0.0-rc.0'],
-        ['patch', true, 'rc', 'release/', '0.0.0', '1.2.3-alpha', '1.2.3-rc.0'],
-        ['minor', true, 'rc', 'release/', '0.0.0', '1.2.3-alpha', '1.2.3-rc.0'],
-        ['major', true, 'rc', 'release/', '0.0.0', '1.2.3-alpha', '1.2.3-rc.0'],
-
-        ['patch', false, 'rc', 'release/', '0.0.0', undefined, '0.0.1'],
-        ['minor', false, 'rc', 'release/', '0.0.0', undefined, '0.1.0'],
-        ['major', false, 'rc', 'release/', '0.0.0', undefined, '1.0.0'],
-
-        ['patch', false, 'rc', 'release/', '0.0.0', '1.0.0', '1.0.1'],
-        ['minor', false, 'rc', 'release/', '0.0.0', '1.0.0', '1.1.0'],
-        ['major', false, 'rc', 'release/', '0.0.0', '1.0.0', '2.0.0'],
-        ['patch', false, 'rc', 'release/', '0.0.0', '1.2.3', '1.2.4'],
-        ['minor', false, 'rc', 'release/', '0.0.0', '1.2.3', '1.3.0'],
-        ['major', false, 'rc', 'release/', '0.0.0', '1.2.3', '2.0.0'],
-        ['patch', false, 'rc', 'release/', '0.0.0', '1.0.0-rc.0', '1.0.0'],
-        ['minor', false, 'rc', 'release/', '0.0.0', '1.0.0-rc.0', '1.0.0'],
-        ['major', false, 'rc', 'release/', '0.0.0', '1.0.0-rc.0', '1.0.0'],
-        ['patch', false, 'rc', 'release/', '0.0.0', '1.2.3-rc.4', '1.2.3'],
-        ['minor', false, 'rc', 'release/', '0.0.0', '1.2.3-rc.4', '1.3.0'],
-        ['major', false, 'rc', 'release/', '0.0.0', '1.2.3-rc.4', '2.0.0'],
-
-        ['patch', false, 'rc', 'release/', '0.0.0', '1.0.0-alpha.0', '1.0.0'],
-        ['minor', false, 'rc', 'release/', '0.0.0', '1.0.0-alpha.0', '1.0.0'],
-        ['major', false, 'rc', 'release/', '0.0.0', '1.0.0-alpha.0', '1.0.0'],
-        ['patch', false, 'rc', 'release/', '0.0.0', '1.2.3-alpha.4', '1.2.3'],
-        ['minor', false, 'rc', 'release/', '0.0.0', '1.2.3-alpha.4', '1.3.0'],
-        ['major', false, 'rc', 'release/', '0.0.0', '1.2.3-alpha.4', '2.0.0'],
-        ['patch', false, 'rc', 'release/', '0.0.0', '1.0.0-alpha', '1.0.0'],
-        ['minor', false, 'rc', 'release/', '0.0.0', '1.0.0-alpha', '1.0.0'],
-        ['major', false, 'rc', 'release/', '0.0.0', '1.0.0-alpha', '1.0.0'],
-        ['patch', false, 'rc', 'release/', '0.0.0', '1.2.3-alpha', '1.2.3'],
-        ['minor', false, 'rc', 'release/', '0.0.0', '1.2.3-alpha', '1.3.0'],
-        ['major', false, 'rc', 'release/', '0.0.0', '1.2.3-alpha', '2.0.0'],
-    ],
-}
-
-describe('findMostRecentVersion()', () => {
-    test.each(testCases.findMostRecentVersion)(
-        'when (arg0=%p) and the most recent tags found are %p then returns %p',
-        async (tagPrefix, foundTags, expected) => {
-            const oneTagPerLine = (foundTags as string[]).join('\n')
-            mockSimpleGit.tag.mockResolvedValueOnce(oneTagPerLine)
-
-            const result = await findMostRecentVersion(tagPrefix as string)
-
-            expect(result).toEqual(expected)
-        },
-    )
+        expect(result).toEqual(expected)
+    })
 })
 
 describe('bumpVersion()', () => {
-    test.each(testCases.bumpVersion)(
-        'when (arg0=%p, arg1=%p, arg2=%p, arg3=%p, arg4=%p) and the most recent version is %p then returns %p',
-        async (type, prerelease, prereleaseId, tagPrefix, fallbackVersion, recentVersion, expected) => {
-            jest.spyOn(version, 'findMostRecentVersion').mockResolvedValueOnce(recentVersion as string | undefined)
+    test.each([
+        [['patch', true, 'rc', '', []], undefined],
 
+        // No versions
+        [['patch', true, 'rc', '0.0.0', []], '0.0.1-rc.0'],
+        [['minor', true, 'rc', '0.0.0', []], '0.1.0-rc.0'],
+        [['major', true, 'rc', '0.0.0', []], '1.0.0-rc.0'],
+
+        // No Release
+        [['patch', true, 'rc', '0.0.0', ['1.2.0-rc.0']], '0.0.1-rc.0'],
+        [['minor', true, 'rc', '0.0.0', ['1.2.0-rc.0']], '0.1.0-rc.0'],
+        [['major', true, 'rc', '0.0.0', ['1.2.0-rc.0']], '1.0.0-rc.0'],
+        [['patch', true, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.1-rc.0']], '0.0.1-rc.0'],
+        [['minor', true, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.1-rc.0']], '0.1.0-rc.0'],
+        [['major', true, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.1-rc.0']], '1.0.0-rc.0'],
+
+        // No Prerelease
+        [['patch', true, 'rc', '0.0.0', ['1.2.0']], '1.2.1-rc.0'],
+        [['minor', true, 'rc', '0.0.0', ['1.2.0']], '1.3.0-rc.0'],
+        [['major', true, 'rc', '0.0.0', ['1.2.0']], '2.0.0-rc.0'],
+        [['patch', true, 'rc', '0.0.0', ['1.2.0', '1.2.1']], '1.2.2-rc.0'],
+        [['minor', true, 'rc', '0.0.0', ['1.2.0', '1.2.1']], '1.3.0-rc.0'],
+        [['major', true, 'rc', '0.0.0', ['1.2.0', '1.2.1']], '2.0.0-rc.0'],
+
+        // Release > Prerelease
+        [['patch', true, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.0']], '1.2.1-rc.0'],
+        [['minor', true, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.0']], '1.3.0-rc.0'],
+        [['major', true, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.0']], '2.0.0-rc.0'],
+        [['patch', true, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.0-rc.1', '1.2.0']], '1.2.1-rc.0'],
+        [['minor', true, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.0-rc.1', '1.2.0']], '1.3.0-rc.0'],
+        [['major', true, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.0-rc.1', '1.2.0']], '2.0.0-rc.0'],
+
+        // Prerelease > Release
+        [['patch', true, 'rc', '0.0.0', ['1.2.0', '1.3.0-rc.0']], '1.2.1-rc.0'],
+        [['minor', true, 'rc', '0.0.0', ['1.2.0', '1.3.0-rc.0']], '1.3.0-rc.1'],
+        [['major', true, 'rc', '0.0.0', ['1.2.0', '1.3.0-rc.0']], '2.0.0-rc.0'],
+        [['patch', true, 'rc', '0.0.0', ['1.2.0', '1.3.0-rc.0', '1.2.1-rc.0']], '1.2.1-rc.1'],
+        [['minor', true, 'rc', '0.0.0', ['1.2.0', '1.3.0-rc.0', '1.2.1-rc.0']], '1.3.0-rc.1'],
+        [['major', true, 'rc', '0.0.0', ['1.2.0', '1.3.0-rc.0', '1.2.1-rc.0']], '2.0.0-rc.0'],
+
+        // No versions
+        [['patch', false, 'rc', '0.0.0', []], '0.0.1'],
+        [['minor', false, 'rc', '0.0.0', []], '0.1.0'],
+        [['major', false, 'rc', '0.0.0', []], '1.0.0'],
+
+        // No Release
+        [['patch', false, 'rc', '0.0.0', ['1.2.0-rc.0']], '0.0.1'],
+        [['minor', false, 'rc', '0.0.0', ['1.2.0-rc.0']], '0.1.0'],
+        [['major', false, 'rc', '0.0.0', ['1.2.0-rc.0']], '1.0.0'],
+        [['patch', false, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.1-rc.0']], '0.0.1'],
+        [['minor', false, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.1-rc.0']], '0.1.0'],
+        [['major', false, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.1-rc.0']], '1.0.0'],
+
+        // No Prerelease
+        [['patch', false, 'rc', '0.0.0', ['1.2.0']], '1.2.1'],
+        [['minor', false, 'rc', '0.0.0', ['1.2.0']], '1.3.0'],
+        [['major', false, 'rc', '0.0.0', ['1.2.0']], '2.0.0'],
+        [['patch', false, 'rc', '0.0.0', ['1.2.0', '1.2.1']], '1.2.2'],
+        [['minor', false, 'rc', '0.0.0', ['1.2.0', '1.2.1']], '1.3.0'],
+        [['major', false, 'rc', '0.0.0', ['1.2.0', '1.2.1']], '2.0.0'],
+
+        // Release > Prerelease
+        [['patch', false, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.0']], '1.2.1'],
+        [['minor', false, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.0']], '1.3.0'],
+        [['major', false, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.0']], '2.0.0'],
+        [['patch', false, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.0-rc.1', '1.2.0']], '1.2.1'],
+        [['minor', false, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.0-rc.1', '1.2.0']], '1.3.0'],
+        [['major', false, 'rc', '0.0.0', ['1.2.0-rc.0', '1.2.0-rc.1', '1.2.0']], '2.0.0'],
+
+        // Prerelease > Release
+        [['patch', false, 'rc', '0.0.0', ['1.2.0', '1.3.0-rc.0']], '1.2.1'],
+        [['minor', false, 'rc', '0.0.0', ['1.2.0', '1.3.0-rc.0']], '1.3.0'],
+        [['major', false, 'rc', '0.0.0', ['1.2.0', '1.3.0-rc.0']], '2.0.0'],
+        [['patch', false, 'rc', '0.0.0', ['1.2.0', '1.3.0-rc.0', '1.2.1-rc.0']], '1.2.1'],
+        [['minor', false, 'rc', '0.0.0', ['1.2.0', '1.3.0-rc.0', '1.2.1-rc.0']], '1.3.0'],
+        [['major', false, 'rc', '0.0.0', ['1.2.0', '1.3.0-rc.0', '1.2.1-rc.0']], '2.0.0'],
+
+        // Mismatch prereleaseid
+        [['patch', true, 'rc', '0.0.0', ['0.1.0-alpha.0']], '0.0.1-rc.0'],
+        [['minor', true, 'rc', '0.0.0', ['0.1.0-alpha.0']], '0.1.0-rc.0'],
+        [['major', true, 'rc', '0.0.0', ['0.1.0-alpha.0']], '1.0.0-rc.0'],
+        [['patch', true, 'rc', '0.0.0', ['0.1.0-alpha.0', '0.0.1-alpha.0']], '0.0.1-rc.0'],
+        [['minor', true, 'rc', '0.0.0', ['0.1.0-alpha.0', '0.0.1-alpha.0']], '0.1.0-rc.0'],
+        [['major', true, 'rc', '0.0.0', ['0.1.0-alpha.0', '0.0.1-alpha.0']], '1.0.0-rc.0'],
+    ])(
+        'when args=%j then returns %p',
+        async ([type, prerelease, prereleaseId, fallbackVersion, versions], expected) => {
             const result = await bumpVersion(
                 type as Type,
                 prerelease as Prerelease,
                 prereleaseId as PrereleaseId,
-                tagPrefix as TagPrefix,
                 fallbackVersion as string,
+                versions as string[],
             )
 
             expect(result).toEqual(expected)
--- .github/actions/bump-semantic-version/src/version.ts
+++ .github/actions/bump-semantic-version/src/version.ts
@@ -3,7 +3,19 @@ import semver from 'semver'
 import { simpleGit } from 'simple-git'
 import { PrereleaseId, Prerelease, TagPrefix, Type, FallbackVersion } from './input'
 
-export async function findMostRecentVersion(tagPrefix: string): Promise<string | undefined> {
+function getSemanticVersions(tagNames: string[], tagPrefix?: string): string[] {
+    return tagNames
+        .flatMap((tagName) =>
+            tagPrefix != null && tagPrefix.length > 0
+                ? tagName.startsWith(tagPrefix)
+                    ? tagName.substring(tagPrefix.length)
+                    : []
+                : tagName,
+        )
+        .filter((tag) => semver.valid(tag))
+}
+
+export async function findVersions(tagPrefix?: TagPrefix): Promise<string[]> {
     const git = simpleGit()
 
     const describeResult = await git.raw([
@@ -12,52 +24,94 @@ export async function findMostRecentVersion(tagPrefix: string): Promise<string |
         '--abbrev=0',
         '--first-parent',
         '--always',
-        ...(tagPrefix?.length > 0 ? [`--match="${tagPrefix}**"`] : []),
+        ...(tagPrefix != null && tagPrefix?.length > 0 ? [`--match="${tagPrefix}**"`] : []),
         'HEAD',
     ])
 
     const commit = describeResult?.trim()
-    const tagResult = await git.tag(['--points-at', commit])
-    core.debug(`tag-result: ${JSON.stringify(tagResult)}`)
+    const revListResult = await git.raw(['rev-list', '--first-parent', '--simplify-by-decoration', commit])
 
-    const tags = tagResult
-        .split(/\s/)
-        .flatMap((tagName) =>
-            tagPrefix?.length > 0
-                ? tagName.startsWith(tagPrefix)
-                    ? tagName.substring(tagPrefix.length)
-                    : []
-                : tagName,
-        )
+    const commits = revListResult.split(/\s/g).filter((c) => !!c)
+    const tagResult = await git.raw(['tag', ...commits.flatMap((commit) => ['--points-at', commit])])
+
+    const tags = tagResult.split(/\s/g)
     core.debug(`tags: ${JSON.stringify(tags)}`)
 
-    const recentVersion = semver.maxSatisfying(tags, '*', {
-        includePrerelease: true,
-    })
-    if (recentVersion == null) {
-        core.info(`no recent tag found at ${commit}`)
-        return
-    }
+    const versions = getSemanticVersions(tags, tagPrefix)
+    core.debug(`versions: ${JSON.stringify(versions)}`)
 
-    core.info(`recent tag '${recentVersion}' found at ${commit}`)
-    return recentVersion
+    return versions
 }
 
 export async function bumpVersion(
     type: Type,
     prerelease: Prerelease,
     prereleaseId: PrereleaseId,
-    tagPrefix: TagPrefix,
     fallbackVersion: FallbackVersion,
+    versions: string[],
 ): Promise<string | undefined> {
-    const recentVersion = await findMostRecentVersion(tagPrefix)
+    // 1. Find max release version
+    core.debug(`step: 1`)
+    const releaseVersion = semver.maxSatisfying([fallbackVersion, ...versions], '*') ?? fallbackVersion
+    core.info(`release-version: ${JSON.stringify(releaseVersion)}`)
 
-    const baseVersion = recentVersion == null ? fallbackVersion : recentVersion
-    const isPrerelease = semver.prerelease(baseVersion) != null
+    // 2. Find max pre-release version of given choice based on (1)
+    core.debug(`step: 2`)
+    const prereleaseVersion =
+        semver.maxSatisfying([fallbackVersion, ...versions], '*', {
+            includePrerelease: true,
+        }) ?? fallbackVersion
+    core.info(`prerelease-version: ${JSON.stringify(prereleaseVersion)}`)
 
-    const bumpVersion = prerelease
-        ? semver.inc(baseVersion, isPrerelease ? 'prerelease' : `pre${type}`, false, prereleaseId)
-        : semver.inc(baseVersion, type)
+    // 3. Bump version of given `type`, `prerelease` based on (1)
+    core.debug(`step: 3`)
+    const nextVersion = semver.inc(releaseVersion, prerelease ? `pre${type}` : type, false, prereleaseId)
+    if (nextVersion == null) {
+        return
+    }
 
-    return bumpVersion ?? undefined
+    // 4. If (1) and (2) is equals, return (3)
+    core.debug(`step: 4`)
+    if (releaseVersion === prereleaseVersion) {
+        return nextVersion ?? undefined
+    }
+
+    // 5. If given `prerelease=false`, return (3)
+    core.debug(`step: 5`)
+    if (!prerelease) {
+        return nextVersion ?? undefined
+    }
+
+    // 6. Find max pre-release version from major/minor/patch of (3)
+    //     - for `major` type, `major.0.0`
+    //     - for `minor` type, `major.minor.0`
+    //     - for `patch` type, `major.minor.patch`
+    core.debug(`step: 6`)
+    let maxPrerelease: string | undefined
+    const major = semver.major(nextVersion)
+    const minor = semver.minor(nextVersion)
+    const patch = semver.patch(nextVersion)
+    if (type === 'major') {
+        maxPrerelease =
+            semver.maxSatisfying(versions, `~${major}.0.0-${prereleaseId}.0`, { includePrerelease: true }) ?? undefined
+    } else if (type === 'minor') {
+        maxPrerelease =
+            semver.maxSatisfying(versions, `~${major}.${minor}.0-${prereleaseId}.0`, { includePrerelease: true }) ??
+            undefined
+    } else if (type === 'patch') {
+        maxPrerelease =
+            semver.maxSatisfying(versions, `~${major}.${minor}.${patch}-${prereleaseId}.0`, {
+                includePrerelease: true,
+            }) ?? undefined
+    }
+
+    // 7. If (6) not exists, return (3)
+    core.debug(`step: 7`)
+    if (maxPrerelease == null) {
+        return nextVersion
+    }
+
+    // 8. Bump version of given `type` with `prerelease=true` based on (6), return (8)
+    core.debug(`step: 8`)
+    return semver.inc(maxPrerelease, prerelease ? 'prerelease' : type, false, prereleaseId) ?? undefined
 }
